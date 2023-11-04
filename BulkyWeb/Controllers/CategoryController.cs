0bf1176f544f9fd40d0b7026cdd46790a399f352
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2023-11-04 14:30:59 +0200

    Update Categories.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -35,5 +35,35 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb2 = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb3 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name != null && category.Name.ToLower() == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "Name and Display Order cannot be exactly the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
