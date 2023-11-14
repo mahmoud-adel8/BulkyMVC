60b18bbe68c74a20ba25b2c34a4dd7a52f9abeba
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2023-11-14 11:03:47 +0200

    Create Product Model.

---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Range(0, 1000)]
+        [Display(Name = "List Price")]
+        public double ListPrice { get; set; }
+        [Required]
+        [Range(0, 1000)]
+        [Display(Name = "Price for 1-50")]
+        public double price { get; set; }
+        [Required]
+        [Range(0, 1000)]
+        [Display(Name = "Price for 50+")]
+        public double price50 { get; set; }
+        [Required]
+        [Range(0, 1000)]
+        [Display(Name = "Price for 100+")]
+        public double price100 { get; set; }
+    }
+}
