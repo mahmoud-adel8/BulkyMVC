4e15ce776246711722780712e1f28b199a5c3498
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2023-11-26 14:38:27 +0200

    Add Roles to DB.

---
+++ Bulky.Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace Bulky.Utility
+{
+    public class SD
+    {
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+        public static string Role_Company = "Company";
+        public static string Role_Employee = "Employee";
+    }
+}
