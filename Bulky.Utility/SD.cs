719c21fea02a4e3b0d3e20905b610bb9b1ce77cc
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2023-11-27 12:03:41 +0200

    Add Authorization to Category & Product.

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -1,10 +1,10 @@
 ﻿namespace Bulky.Utility
 {
-    public class SD
+    public static class SD
     {
-        public static string Role_Admin = "Admin";
-        public static string Role_Customer = "Customer";
-        public static string Role_Company = "Company";
-        public static string Role_Employee = "Employee";
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Employee = "Employee";
     }
 }
