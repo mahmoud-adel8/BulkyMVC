c9a1811214dd512b39e2b1be1d8261006f890558
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2023-11-29 14:07:44 +0200

    Bug Fix: Implementing IRepository.

--- Bulky.DataAccess/Repository/IRepository/ICompanyRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/ICompanyRepository.cs
@@ -2,7 +2,7 @@
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    public interface ICompanyRepository
+    public interface ICompanyRepository : IRepository<Company>
     {
         void Update(Company company);
     }
